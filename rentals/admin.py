443136659aa7846f69a18798f49fbac46b13c7ec
saa81 <saadatakmatova122@gmail.com>
2025-04-30 15:04:21 +0300

    firt

---
+++ rentals/admin.py
@@ -0,0 +1,22 @@
+from msilib.schema import Property
+
+from django.contrib import admin
+from django.template.defaultfilters import title
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'address', 'price_per_night', 'created_at')
+
+
+class Booking:
+    pass
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'property', 'start_date', 'end_date', 'created_at')
+    
+
+    
+
---
+++ rentals/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals')
+    title = models.CharField(max_length=99)
+    description= models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def progress_percent(self):
+        steps = self.steps.all()
+        if not steps.exists():
+            return 0
+        done_steps = steps.filter(is_done=True).count()
+        return int((done_steps / steps.count()) * 100)
+    def status(self):
+        steps = self.steps.all()
+        if not steps.exists():
+            return'not_started'
+        done_count = steps.filter(is_done=True).count()
+        if done_count == 0:
+            return 'not_started'
+        elif done_count == steps.count():
+            return 'done'
+        else:
+            return 'in_progress'
+
+    def __str__(self):
+        return self.title
+class Step(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name='steps')
+    title = models.CharField(max_length=99)
+    is_done = models.BooleanField(default=False)
+    due_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} ({'Done' if self.is_done else 'Pending'})"
+
+
+
+
+
+
+
+
+
+
+
+
+
